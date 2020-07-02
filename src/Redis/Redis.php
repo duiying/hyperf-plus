822b92202471f1593417e954f12925640ea56d81
wangyaxian <wangyaxian@higohappy.com>
2020-07-02 18:53:04 +0800

    feat:redis

--- src/Redis/Redis.php
+++ src/Redis/Redis.php
@@ -13,10 +13,50 @@ use Hyperf\Utils\ApplicationContext;
  */
 class Redis
 {
-    public static function set($key, $value, $timeout = null, $poolName = 'default')
+
+    /******************** string begin ********************/
+    public function set($key, $value, $timeout = null, $poolName = 'default')
     {
         $container = ApplicationContext::getContainer();
         $redis = $container->get(RedisFactory::class)->get($poolName);
         return $redis->set($key, $value, $timeout);
     }
+
+    public function setNx($key, $value, $timeout = 1, $poolName = 'default')
+    {
+        $container = ApplicationContext::getContainer();
+        $redis = $container->get(RedisFactory::class)->get($poolName);
+        return $redis->set($key, $value, ['nx', 'ex' => $timeout]);
+    }
+    /******************** string end ********************/
+
+    /******************** list begin ********************/
+    public function lPush($key, $value, $poolName = 'default')
+    {
+        $container = ApplicationContext::getContainer();
+        $redis = $container->get(RedisFactory::class)->get($poolName);
+        return $redis->lPush($key, $value);
+    }
+
+    public function lPop($key, $poolName = 'default')
+    {
+        $container = ApplicationContext::getContainer();
+        $redis = $container->get(RedisFactory::class)->get($poolName);
+        return $redis->lPop($key);
+    }
+
+    public function rPush($key, $value, $poolName = 'default')
+    {
+        $container = ApplicationContext::getContainer();
+        $redis = $container->get(RedisFactory::class)->get($poolName);
+        return $redis->rPush($key, $value);
+    }
+
+    public function rPop($key, $poolName = 'default')
+    {
+        $container = ApplicationContext::getContainer();
+        $redis = $container->get(RedisFactory::class)->get($poolName);
+        return $redis->rPop($key);
+    }
+    /******************** list end ********************/
 }
